845ae0df56e1afb9fc255ae03d0cd6c3e158a02c
zhangheng <zhangheng@pateo.com.cn>
2018-07-13 18:02:25 +0800

    新增代码块

---
+++ video-barrage/src/components/User.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import logo from '../assets/logo.svg';
+import '../styles/App.css';
+
+class User extends Component {
+    render() {
+        return (
+            <div style={{height:100,width:100,color:'red'}} >
+                测试
+            </div>
+        );
+    }
+}
+
+export default User;
