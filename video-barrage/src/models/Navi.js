0f13322a0a17e9563c5ca11a4d86b68a57ab10a6
zhangheng <zhangheng@pateo.com.cn>
2018-07-18 18:27:45 +0800

    新增model层

---
+++ video-barrage/src/models/Navi.js
@@ -0,0 +1,21 @@
+export default {
+
+    namespace: 'Navi',
+
+    state: {
+        name:'这是Navi的model'
+    },
+
+    subscriptions: {
+
+    },
+
+    effects: {
+
+    },
+
+    reducers: {
+
+    },
+
+};
