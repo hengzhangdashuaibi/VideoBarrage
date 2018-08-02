0f2cc1bd2d4a425d2aaf5de1f09e168a5d502e56
zhangheng <zhangheng@pateo.com.cn>
2018-08-02 17:50:01 +0800

    添加h5-video的依赖

--- video-barrage/src/router.js
+++ video-barrage/src/router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 // import dynamic from 'dva/dynamic';
 import Navi from './routes/Navi';
+import Login from './routes/Login'
 
 // const menuGlobal=[
 //     // {
@@ -46,7 +47,8 @@ function RouterConfig({ history, app }) {
 
            <div>
                 <Switch>
-                    <Route path="/" render={props => <Navi {...props} {...passProps} />}/>
+                    <Route path="/" render={props => <Login {...props} {...passProps} />}/>
+                    {/*<Route path="/" render={props => <Navi {...props} {...passProps} />}/>*/}
                 </Switch>
             </div>
         </Router>
