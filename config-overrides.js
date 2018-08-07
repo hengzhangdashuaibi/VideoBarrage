1dd6e1b7d974c74970aea67f554bffa49e64bf2e
alixez <alixe.z@foxmail.com>
2018-08-07 09:41:30 +0800

    fix doc

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,8 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 
 module.exports = function override(config, env) {
-     config = injectBabelPlugin(["transform-decorators-legacy","transform-class-properties"], config);
+    config = injectBabelPlugin(["transform-decorators-legacy","transform-class-properties"], config);
+
+    
     return config;
 };
